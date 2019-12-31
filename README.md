106a0cb9d5f177b39585ee86935c1df605982f24
rvasseur31 <raphael.vasseur@ynov.com>
2019-12-31 14:47:36 +0100

    Add readme + start service on boot

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# endless-service-java
+# Endless-service-java
+
+This project show you how to run a endless service with java.
+
+I realized it thanks to this project : 
+- https://github.com/robertohuertasm/endless-service
--- app/src/main/java/com/example/myapplication/StartReceiver.java
+++ app/src/main/java/com/example/myapplication/StartReceiver.java
@@ -1,4 +1,25 @@
 package com.example.myapplication;
 
-public class StartReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import java.util.Objects;
+
+public class StartReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED) && new ServiceTracker().getServiceState(context) == ServiceState.STARTED) {
+            intent = new Intent(context, EndlessService.class);
+            intent.setAction(Actions.START.name());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                new log("Starting the service in >=26 Mode");
+                context.startForegroundService(intent);
+                return;
+            }
+            new log("Starting the service in < 26 Mode");
+            context.startService(intent);
+        }
+    }
 }
